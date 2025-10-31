e3fa36b636638199cbf6d1e5d50b4a22ae3c66a7
Ankushkumardas <anksuh114@gmail.com>
2025-10-31 10:25:10 +0530

    1.0.5

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -1,13 +1,33 @@
+import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import autoprefixer from "autoprefixer";
+
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
+
   css: {
     postcss: {
       plugins: [autoprefixer()],
     },
+  },
+
+  build: {
+    outDir: "dist", // fallback for safety
+    emptyOutDir: true,
+  },
+
+  server: {
+    port: 5173,
+    open: true,
+  },
+
+  preview: {
+    port: 4173,
   },
 });
